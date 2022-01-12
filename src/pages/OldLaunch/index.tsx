dda5e60188a0c81a30cdb7dce33a706165e4580b
Anderson Silva <andssilva.23@gmail.com>
2022-01-12 10:14:28 -0400

    feat: :rocket: renderiza os dados de lançamentos passados

--- src/pages/OldLaunch/index.tsx
+++ src/pages/OldLaunch/index.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import LaunchCard from '../../components/LaunchCard';
+import apiSpaceX from '../../services/api';
+import { Container, SectionContainer } from './styles';
+
+interface GetDataProps {
+  links: {
+    patch: {
+      small: string,
+    },
+    wikipedia: string,
+  },
+  name: string,
+}
 
 function OldLaunch() {
+  const [past, setPast] = useState<GetDataProps[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const getPast = await apiSpaceX.get('/past');
+      setPast(getPast.data);
+    };
+
+    getData();
+  }, []);
   return (
-    <div>
-      OldLaunch
-    </div>
+    <Container>
+      <h1>Lançamentos Passados</h1>
+      <SectionContainer>
+        {past && past.map((pastLaunches) => (
+          <LaunchCard key={pastLaunches.name} launch={pastLaunches} />
+        ))}
+      </SectionContainer>
+    </Container>
   );
 }
 
---
+++ src/pages/OldLaunch/styles.ts
@@ -0,0 +1,18 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  ${() => css`
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 2rem 1rem 12rem;
+    margin-top: -150px;
+  `}
+`;
+
+export const SectionContainer = styled.section`
+  ${() => css`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 2rem;
+  `}
+`;
