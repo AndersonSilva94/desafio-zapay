c64c73fe28af0fe3fb212bbe3481552bd4e9a657
Anderson Silva <andssilva.23@gmail.com>
2022-01-13 03:47:50 -0400

    fix: :bug: corrige teste de Header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import Header from '.';
 
 describe('<Header />', () => {
-  it('Se Header tem uma logo com o texto SpaceX', () => {
-    const { getByText } = render(<Header />);
-    expect(getByText('SpaceX')).toBeInTheDocument();
+  it('Se Header tem uma logo', () => {
+    const { getByRole } = render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+    );
+    const img = getByRole('img') as HTMLImageElement;
+    expect(img).toBeInTheDocument();
+    expect(img.alt).toBe('logo');
   });
+
   it('Se Header possui um elemento Nav', () => {
-    const { getByRole } = render(<Header />);
+    const { getByRole } = render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+    );
     expect(getByRole('navigation')).toBeInTheDocument();
   });
+
   it('Possui os links Home, Lançamentos Passados e Próximos Lançamentos', () => {
-    const { getAllByRole } = render(<Header />);
+    const { getAllByRole } = render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+    );
     const links = getAllByRole('link');
     expect(links[0].innerHTML).toBe('Home');
     expect(links[1].innerHTML).toBe('Lançamentos Passados');
